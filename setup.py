f56e865c419af58595cc99e2ee879ef54206100c
Göktuğ Karakaşlı <20567087+goktug97@users.noreply.github.com>
2019-12-25 21:24:53 +0300

    fix numpy include

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 import os
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
 directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
@@ -11,16 +12,17 @@ with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='dynamic-window-approach',
     description='Dynamic Window Approach algorithm written in C with Python Bindings',
-    version='1.0.1',
+    version='1.0.2',
     author='Göktuğ Karakaşlı',
     author_email='karakasligk@gmail.com',
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    ext_modules = cythonize([Extension("dwa", ["python/dwa.pyx"])]),
+    ext_modules = cythonize([Extension("dwa", ["python/dwa.pyx"],
+        include_dirs=[numpy.get_include(), 'src'])]),
     url='https://github.com/goktug97/DynamicWindowApproach',
     download_url=(
-        'https://github.com/goktug97/DynamicWindowApproach/archive/v1.0.1.tar.gz'),
+        'https://github.com/goktug97/DynamicWindowApproach/archive/v1.0.2.tar.gz'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
